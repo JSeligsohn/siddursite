fbe8cff742e90ffe9ac110bd622a88c9ba70be87
Jonathan Seligsohn <jbseligsohn@gmail.com>
2014-07-10 18:02:45 -0400

    fixed nav collapse for mobile after selecting link and fixed spacing of special text on mobiel

--- js/app.js
+++ js/app.js
@@ -8,5 +8,9 @@ $('.expand').click(function() {
 	$(this).next().slideToggle();
 });
 
-
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
 
