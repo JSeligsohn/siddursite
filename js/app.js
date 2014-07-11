431ef982535c86aa31dd2747fc979505a4a27dfd
Jonathan Seligsohn <jbseligsohn@gmail.com>
2014-07-11 13:06:06 -0400

    added kedushah and fixed anchors to scrollto top with proper spacing

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
 });
 
 $('.expand').click(function() {
+	$(this).children().toggleClass('rotate');
 	$(this).next().slideToggle();
 });
 
@@ -14,4 +15,25 @@ $(document).on('click','.navbar-collapse.in',function(e) {
     }
 });
 
+// $('.dropdown > a').click(function(target) {
+// 	console.log('scrolltop!');
+// 	target.scrollTop(300);
+// })
+
+//Smooth Scrolling to anchor
+	$(function() {
+	  $('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	      	$('html,body').scrollTop(target.offset().top -55);
+	        // $('html,body').animate({
+	        //   scrollTop: target.offset().top -55
+	        // }, 300);
+	        return false;
+	      }
+	    }
+	  });
+	});
 
